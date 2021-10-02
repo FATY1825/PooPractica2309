f992ffce26fd23c6b866586e2582d7c3e152e0d0
Fatima Ayala <ftayala89@gmail.com>
2021-10-02 09:35:26 -0600

    POO

---
+++ PooPractica2309/Cliente.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooPractica2309
+{
+    class Cliente
+    {
+        //definir mienbros de la clase
+        //Propiedades|Atributos|Datos 
+
+        private int _clienteId;
+        private string _names;
+        public string _address { get; set; }
+        private int _telephon;
+        private int _mobile;
+
+        //metodo constructor
+        public Cliente(int clienteId, string names, 
+            string address, int telephon, int mobile) //metodo constructor vacio
+        {
+            _clienteId = clienteId;
+            _names = names;
+            _address = address;
+            _telephon = telephon;
+            _mobile = mobile;
+        }
+
+        //metodo de propiedad
+        //get -- obtener el valor almacenado en la propiedad
+
+        //set -- asignar el valor a la variable de propiedad
+
+        public void setClienteId (int clienteId)
+        {
+            _clienteId = clienteId;
+        }
+
+        public int getClienteId()
+        {
+            return _clienteId;
+        }
+
+        public void setNames (string names)
+        {
+            _names = names;
+        }
+
+        public string getNames()
+        {
+            return _names;
+        }
+
+        public void setTelephon (int telephon)
+        {
+            _telephon = telephon;
+        }
+
+        public int getTelephon()
+        {
+            return _telephon;
+        }
+
+        public void setMobile (int mobile)
+        {
+            _mobile = mobile;
+        }
+
+        public int getMobile()
+        {
+            return _mobile;
+        }
+    }
+
+}
---
+++ PooPractica2309/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PooPractica2309
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Instancia de la clase cliente 
+
+            Cliente cliente1 = new Cliente( 0001, "Jose Natanael Ayala Castellon ", 
+                "Ave. 23 de enero, Uluazapa, San Miguel", 26193678, 77546792);
+
+            Cliente cliente2 = new Cliente(0002, "Margarita Rosibel Garcia Cuadros",
+                "Colonia Santa Clara, San Bernardo", 26879234, 78549312);
+
+
+            //Mostrar datos del cliente1
+             MessageBox.Show(cliente1.getClienteId() + cliente1.getNames() + cliente1._address + cliente1.getTelephon() 
+                 + cliente1.getMobile())
+
+            //Mediante metodo de propiedad 
+            //creamos objeto cliente
+            //cliente.setClienteId(0001);
+            //cliente.setNames("Jose Natanael Ayala Castellon");
+
+            //Mostrar los datos 
+            //MessageBox.Show(cliente.getClienteId() + " " +cliente.getNames());
+
+            //PROPIEDAD AUTOIMPLEMENTADA
+            //DIRECCION
+            //SET
+            //cliente._address = "Ave. 23 de enero, Uluazapa, San Miguel";
+
+            //GET
+            //MessageBox.Show(cliente._address);
+
+            //Metodo de propiedad 
+            //cliente.setTelephon(26193678);
+            //cliente.setMobile(77546792);
+
+            //Mostrar los datos 
+            //MessageBox.Show(cliente.getTelephon() + " " + cliente.getMobile())
+;        }
+    }
+}
